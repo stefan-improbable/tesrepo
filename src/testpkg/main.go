d7b1c218abd4b3b48d499abd9928cf9753bae5b8
Stefan Sakalik <stefan@improbable.io>
2018-01-23 19:29:35 +0000

    rm pkg and stuff

--- src/testpkg/main.go
+++ src/testpkg/main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	"k8s.io/client-go/pkg/api/v1"
+	// Only in old k8s at e121606b0d09b2e1c467183ee46217fa85a6b672.
+	"k8s.io/client-go/pkg/util/intstr"
 )
 
 func main() {
-	fmt.Printf("vim-go: %v", v1.TLSCertKey)
+	fmt.Printf("vim-go: %v, %v", v1.TLSCertKey, intstr.String)
 }
